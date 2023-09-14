82e89fc1ecc0d9492ab58e0956db8df9bb19a125
KappingGit <130461538+KappingGit@users.noreply.github.com>
2023-09-14 17:23:44 +0900

    9월 14일 카메라 연출 작업
    연출 테스트 작업 공간 제작
    
    =>아직 구현 중, Mathf함수를 활용하기

---
+++ Assets/2.Script/DirectCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectCamera : MonoBehaviour
+{
+    // 메인씬 연출용 카메라 스크립트
+    
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
