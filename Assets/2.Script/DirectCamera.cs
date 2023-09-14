3b33aa7360cd9ed19fae41ca19fd8d3a1b15e093
KappingGit <130461538+KappingGit@users.noreply.github.com>
2023-09-14 22:24:38 +0900

    9월 14일 카메라 연출 작업
    신전에 사용될 연출 작업 테스트
    => 얼추 완성은 했지만 Mathf.레퍼런스 클래스를 사용해서 코드 리뷰가 필수로 해야함

--- Assets/2.Script/DirectCamera.cs
+++ Assets/2.Script/DirectCamera.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class DirectCamera : MonoBehaviour
 {
     // 메인씬 연출용 카메라 스크립트
-    
+
+    float circleR; //반지름
+    float deg; //각도
+    float objSpeed = 20f; //원운동 속도
+
     private void Awake()
     {
 
     }
-
+    
+    // 해당 코드 리뷰 필수
     private void Update()
     {
-        
+        deg += Time.deltaTime * objSpeed; // 시간에 따른 원운동 속도
+        if (deg < 360)
+        {
+            var rad = Mathf.Deg2Rad * (deg); // 해당 클래스 레퍼런스는(Mathf.Deg2Rad)는 (PI * 2) / 360와 같은 뜻이다
+            var x = circleR * Mathf.Sin(rad);
+            var y = circleR * Mathf.Cos(rad);
+            
+            transform.position = transform.position + new Vector3(x, y);
+            transform.rotation = Quaternion.Euler(0, deg * -1, 0); //가운데를 바라보게 각도 조절
+        }
+        else
+        {
+            deg = 0;
+        }
     }
 }
