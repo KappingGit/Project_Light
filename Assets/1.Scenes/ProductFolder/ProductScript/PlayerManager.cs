fc1d8cef26019324b3a111e55b316f07e654b2bb
SIM-TH <77562533+SIM-TH@users.noreply.github.com>
2023-10-18 14:54:33 +0900

    서브 스킬 재료 및 코딩 테스트

--- Assets/1.Scenes/ProductFolder/ProductScript/PlayerManager.cs
+++ Assets/1.Scenes/ProductFolder/ProductScript/PlayerManager.cs
@@ -6,6 +6,8 @@ public class PlayerManager : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject slashPrefab;
+    public GameObject subsSkill;
+    public GameObject magic;
     public Transform shotPos;
 
   
@@ -26,6 +28,16 @@ public class PlayerManager : MonoBehaviour
 
             Bullet.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 25), ForceMode.Impulse);
         }
-        
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject Sub = Instantiate(subsSkill);
+            GameObject Magic = Instantiate(magic);
+
+            Sub.transform.position = shotPos.transform.position;
+            Magic.transform.position = shotPos.transform.position;
+            Destroy(Sub.gameObject, 1.4f);
+            Destroy(Magic.gameObject, 1f);
+        }
     }
+
 }
